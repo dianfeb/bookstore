b33420e232de44c3ae6a4a98bcab2aa41a8e7d60
Dian Febrianto <dianfebrianto41@gmail.com>
2024-03-18 23:20:54 +0700

    first commit

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,48 @@
+<div class="sidebar-wrapper active">
+    <div class="sidebar-header">
+        <img src="{{ asset('admin/assets/images/logo.svg') }}" alt="" srcset="">
+    </div>
+    <div class="sidebar-menu">
+        <ul class="menu">
+            <li class='sidebar-title'>Main Menu</li>
+            <li class="sidebar-item ">
+
+                <a href="index.html" class='sidebar-link'>
+                    <i data-feather="home" width="20"></i>
+                    <span>Dashboard</span>
+                </a>
+
+
+            </li>
+
+            <li class="sidebar-item  has-sub">
+
+                <a href="#" class='sidebar-link'>
+                    <i data-feather="briefcase" width="20"></i>
+                    <span>Extra Components</span>
+                </a>
+
+
+                <ul class="submenu ">
+
+                    <li>
+                        <a href="component-extra-avatar.html">Avatar</a>
+                    </li>
+
+                    <li>
+                        <a href="component-extra-divider.html">Divider</a>
+                    </li>
+
+                </ul>
+
+            </li>
+
+
+
+
+
+
+        </ul>
+    </div>
+    <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+</div>
